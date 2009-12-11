004c89d2953459b9de32b83cc95c2ccc5cc5966e
Vincent Petithory <vincent.petithory@gmail.com>
2009-12-11 01:02:15 +0100

    Add po directory where to store translation scripts and po files of the gettext library
     * Delete <files> file (handled in a script instead)
     * Remove locale dir, added po files in ./po
     * Add a script to generate mo files from the po files in the ./po directory

---
+++ po/make_mo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+find . | egrep -i '\.po$' > po
+
+sed -i -e 's/\.po//g' po
+locales=`cat po`
+rm po
+
+out=../lib/locale
+mkdir -p $out
+
+for locale in $locales; do
+    echo "Processing "$locale".po ..."
+    mkdir -p $out/$locale/LC_MESSAGES
+    msgfmt --check --verbose --output-file $out/$locale/LC_MESSAGES/libas3gnugettext.mo $locale.po
+done
